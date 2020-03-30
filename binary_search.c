f80c45392933174355f36d202351b06490217cf9
Sangjin Lee <isangjin77@gmail.com>
2020-03-30 18:39:51 +0900

    binary search in progress

---
+++ binary_search.c
@@ -0,0 +1,48 @@
+#define COMPARE(x,y) ( ((x)<(y))? -1 : ((x)==(y))? 0 : 1 )
+
+//for non-decreasing ordered list
+int binsearch(int list[], int searchnum, int left, int right){
+	while(left <= right){
+		middle = (left + right)/2;
+		switch (COMPARE(list[middle], searchnum)){
+			case -1: left = middle + 1;
+				break;
+			case 0: return middle;
+			case 1: right = middle - 1;
+		}
+	}
+return -1;
+}
+
+//for non-increasing ordered list
+int binsearch(int list[], int searchnum, int left, int right){
+	while(left <= right){
+		middle = (left + right)/2;
+		switch (COMPARE(list[middle], searchnum)){
+			case -1: right = middle - 1;
+				break;
+			case 0: return middle;
+			case 1: left = middle + 1;
+		}
+	}
+return -1;
+}
+
+//modified for assi 2-(b)
+//non-increasing ordered list
+
+#define ADDLIST.....
+
+int binsearch(int list[], int searchnum, int left, int right){
+	while(left <= right){
+		middle = (left + right)/2;
+		switch (COMPARE(list[middle], searchnum)){
+			case -1: right = middle - 1;
+				break;
+			case 0: return middle;
+			case 1: left = middle + 1;
+		}
+	}
+return -1;
+}
+
