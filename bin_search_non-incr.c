be8ac35205c3e1b09be9abc2c957934efac86180
luminus7 <isangjin77@gmail.com>
2020-04-01 05:02:18 +0900

    fix indentations

--- bin_search_non-incr.c
+++ bin_search_non-incr.c
@@ -79,10 +79,10 @@ int binsearch(int list[], int searchnum, int left, int right) {
         int middle;
         middle = (left + right) / 2;
         switch (COMPARE(list[middle], searchnum)) {
-        case -1: right = middle - 1;
-            break;
-        case 0: return middle;
-        case 1: left = middle + 1;
+            case -1: right = middle - 1;
+                break;
+            case 0: return middle;
+            case 1: left = middle + 1;
         }
     }
     return -1;
