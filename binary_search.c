609bd29dbcd4fc30e13569ed64a4aa10caa04949
luminus7 <isangjin77@gmail.com>
2020-04-05 18:39:50 +0900

    'modified version for Assi 2-(b)' included in 'binary_search_mod.c'

--- binary_search.c
+++ binary_search.c
@@ -28,21 +28,3 @@ int binsearch(int list[], int searchnum, int left, int right){
 return -1;
 }
 
-//modified for assi 2-(b)
-//non-increasing ordered list
-
-#define ADDLIST.....
-
-int binsearch(int list[], int searchnum, int left, int right){
-	while(left <= right){
-		middle = (left + right)/2;
-		switch (COMPARE(list[middle], searchnum)){
-			case -1: right = middle - 1;
-				break;
-			case 0: return middle;
-			case 1: left = middle + 1;
-		}
-	}
-return -1;
-}
-
