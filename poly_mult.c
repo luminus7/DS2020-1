83fc229a887d8435f0fe9193a075091d17e13004
luminus7 <isangjin77@gmail.com>
2020-04-08 03:41:23 +0900

    (in progress)Update poly_mult.c
    todo: modify 'binsearch' to find same exp value inside polynomial C

--- poly_mult.c
+++ poly_mult.c
@@ -6,74 +6,139 @@
 #define SWAP(x,y,t) ((t) = (x), (x) = (y), (y) = (t))
 
 typedef struct {
-	int coef; //coefficient, 계수
-	int exp; //exponential, 지수
+	int coef; //coefficient
+	int exp; //exponential
 } polynomial; //구조체 정의와 동시에 polynomial 변수로 선언
 
-void sort (int[], int);
-void printpoly (polynomial* [], int);
+polynomial* A[POLY_SIZE];							 //구조체형 포인터형 전역배열 A[] 선언
+polynomial* B[POLY_SIZE];
+polynomial* C[MAX_SIZE];
+
+void sort(polynomial* [], int);
+void printpoly(polynomial* [], int);
+void multiplypoly(polynomial* [], polynomial* [], int);
 
 int main() {
 	int A_coef[3] = { 2,2,3 }; int A_exp[3] = { 3,2,4 }; // A(x) = 2x^3 + 2x^2 + 3x^4
 	int B_coef[3] = { 7,3,4 }; int B_exp[3] = { 0,1,2 }; // B(x) = 7x^0 + 3x^1 + 4x^2
 
-	polynomial* A[POLY_SIZE]; //구조체형 포인터형 배열 A[] 선언
-	polynomial* B[POLY_SIZE];
-	polynomial* C[MAX_SIZE];
 
-for (int i = 0; i < POLY_SIZE; i++) { //Assign values into polynomial
-	A[i] = malloc(sizeof(polynomial)); //각 포인터형 배열에 구조체 크기만큼 메모리 할당
-	B[i] = malloc(sizeof(polynomial));
-	C[i] = malloc(sizeof(polynomial));
+	for (int i = 0; i < POLY_SIZE; i++) {	//Assign values into polynomial A and B
+		A[i] = malloc(sizeof(polynomial));	//각 포인터형 배열에 구조체 크기만큼 메모리 할당
+		B[i] = malloc(sizeof(polynomial));
 
-	A[i]->coef = A_coef[i]; //만들어진 메모리 공간(구조체)에 기존값 할당
-	B[i]->coef = B_coef[i];
+		A[i]->coef = A_coef[i];				//만들어진 메모리 공간(구조체)에 기존값 할당
+		B[i]->coef = B_coef[i];
+		A[i]->exp = A_exp[i];
+		B[i]->exp = B_exp[i];
+	}
+	for (int i = 0; i < MAX_SIZE; i++) {	//Assign values into polynomial C
+		C[i] = malloc(sizeof(polynomial));
+	}
 
-	A[i]->exp = A_exp[i];
-	B[i]->exp = B_exp[i];
+	printf("--original polynomial A and B--\n");
+	printpoly(A, POLY_SIZE);
+	printpoly(B, POLY_SIZE);
+
+	//use selection sort(non-incr) to sort polynomial A and B
+	for (int i = 0; i < POLY_SIZE; i++) {
+		sort(A, POLY_SIZE);
+		sort(B, POLY_SIZE);
+	}
+	printf("\n--sorted polynomial A and B--\n");
+	printpoly(A, POLY_SIZE);
+	printpoly(B, POLY_SIZE);
+
+	multiplypoly(A, B, POLY_SIZE);
+
+
+
+	return 0;
 }
 
-
-printpoly(A, POLY_SIZE);
-printpoly(B, POLY_SIZE);
-
-//use selection sort(non-incr) to sort polynomial A and B
-for (int i = 0; i < POLY_SIZE; i++) {
-	sort(A[i]->exp, POLY_SIZE);
-}
-printpoly(A, POLY_SIZE);
-
-for (int i = 0; i < 3; i++) { //multiply polynomial and assign result into C[]
-	for (int j = 0; j < 3; j++) {
-
+/*
+void allocpoly_mem(polynomial* list[], int n) {	//Assign values into polynomial
+	for (int i = 0; i < POLY_SIZE; i++) {
+		list[i] = malloc(sizeof(polynomial));
+		list[i]->coef = B_coef[i];
+		list[i]->exp = A_exp[i];
 	}
 }
+*/
 
-
-
-return 0;
-}
-
-
-void sort(int list[], int n) //Sangjin Lee 헤더파일로 빼버리자
-{
+void sort(polynomial* list[], int n) { //Sangjin Lee 헤더파일로 빼버리자
 	int max, temp;
 	for (int i = 0; i < n - 1; i++) {
 		max = i;
 		for (int j = i + 1; j < n; j++) {
-			if (list[max] < list[j])
+			if ((list[max]->exp) < (list[j]->exp))
 				max = j;
 		}
-	SWAP(list[i], list[max], temp);
+		SWAP(list[i], list[max], temp);
 	}
 }
 
-void printpoly(polynomial* list[], int n)
-{
-	for(int i = 0; i < n; i++){
+void printpoly(polynomial* list[], int n) {
+	printf("polynomial: ");
+	for (int i = 0; i < n; i++) {
 		printf("%dx^%d ", list[i]->coef, list[i]->exp);
-		if(i != 2)
-			printf("+ ");	
+		if (i != n - 1)
+			printf("+ ");
 	}
 	printf("\n");
+}
+
+/*
+ *'multiply' polynomial A and B
+ * then 'sort' C (the result of multiplication A and B)
+ * 'search' to find if there is any same exp value in C
+ * if there is same exp value in C, then add coefs to make one polynomial in one term
+ */
+void multiplypoly(polynomial* A_parr[], polynomial* B_parr[], int n) { 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			C[3*i + j]->coef = (A_parr[i]->coef) * (B_parr[j]->coef);
+			C[3*i + j]->exp = (A_parr[i]->exp) + (B_parr[j]->exp);
+		}
+	}
+	printf("\n--before sorting polynomial C--\n");
+	printpoly(C, MAX_SIZE);
+
+	sort(C, MAX_SIZE);
+
+	printf("\n--after sorting polynomial C--\n");
+	printpoly(C, MAX_SIZE);
+
+}
+
+#define COMPARE(x,y) ( ((x)<(y))? -1 : ((x)==(y))? 0 : 1 )
+
+int binsearch(polynomial* list[], int searchnum, int left, int right) { //여기부터어ㅓㅓㅓㅓㅓㅓㅓ~~~
+	int middle = 0;
+	int R = right;
+
+	while (left <= right) {
+		middle = (left + right) / 2;
+		switch (COMPARE(list[middle]->coef, searchnum)) {
+		case -1: left = middle + 1;
+			break;
+		case 0: printf("middle: %d\n", middle); return middle;
+		case 1: right = middle - 1;
+		}
+	}
+	printf("value does not exist\n");
+
+	printf("shift list to right to make emptied memory dest\n");
+
+	int i = 0;
+	while ((list[i] < searchnum) && (list[i] > 0)) i++;   //search proper dest to insert 'searchnum'
+	int dest = i - 1;
+
+	while ((R + 1) != i) {  //shift array elements to the right, until get free space to insert 'searchnum'
+		list[R + 1] = list[R];
+		R--;
+	}
+	list[dest + 1] = searchnum; //insert 'searchnum'
+
+	return -1;
 }
