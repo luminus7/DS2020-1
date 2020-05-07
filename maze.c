a9f3d628fbeb56cd89b46fdb172d919775d31224
luminus7 <isangjin77@gmail.com>
2020-05-08 02:30:26 +0900

    variable name: dir -> direct

--- maze.c
+++ maze.c
@@ -74,14 +74,14 @@ int main() {
 
 
 void path() {
-	int i, row, col, nextRow, nextCol, dir = 0, found = FALSE;	//dir초기화 하는게 88번 줄에도 있는데 시작할때 예외처리해주기 위해 dir을 0으로 둔 것임. 82번 줄을 아래 else문 안에도 넣을 수 있지만 if(dir == 8)... while문 돌때 항상 조사해주는게 overhead가 크다고 생각했음. 근거는...?
+	int i, row, col, nextRow, nextCol, direct = 0, found = FALSE;	//dir초기화 하는게 88번 줄에도 있는데 시작할때 예외처리해주기 위해 dir을 0으로 둔 것임. 82번 줄을 아래 else문 안에도 넣을 수 있지만 if(dir == 8)... while문 돌때 항상 조사해주는게 overhead가 크다고 생각했음. 근거는...?
 	element position;
 	printf("pathfinding...\n");
 	top = 0;
 	stack[0].row = 0;
 	stack[0].col = 0;
 	while (top > -1 && !found) {		//stack에 backtrack할게 남아있고 아직 덜찾았으면 계속 실행, 스택에 더 남아있는게 없으면 종료 아니면 스택과 무관하게 경로 찾았으면 종료...우왕... 논리소자네
-		if (dir >= 8) {
+		if (direct >= 8) {
 			printf("valid move없으니 현위치 바리케이드 치기\n");
 			maze[row][col] = 1;
 		}							/*****내 scheme: pop이 필요해질 때 (blocked상태일때, dir은 8일테니까) 블록된 위치를 1로 바꾸어버림(막아버림)*****/
@@ -89,10 +89,10 @@ void path() {
 		position = pop(&top);
 		row = position.row;
 		col = position.col;
-		dir = 0;
+		direct = 0;
 
-		while (dir < 8 && !found) {		// 시도할 방향있고 아직 덜찾았으면 계속 실행, 시도할 방향이 없어지면 while2 탈출 or 찾았으면 while2 탈출
-			printf("-> dir: %d\n", dir);
+		while (direct < 8 && !found) {		// 시도할 방향있고 아직 덜찾았으면 계속 실행, 시도할 방향이 없어지면 while2 탈출 or 찾았으면 while2 탈출
+			printf("-> dir: %d\n", direct);
 			printf("-> %2d%5d\n\n", row, col);
 			nextRow = row + move[dir].vert;
 			nextCol = col + move[dir].horiz;
